adf1c21d1bba321101615bd03d44355aff4fc597
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2025-06-09 22:51:03 -0300

    V 0.9 - atualizando o store.js colocando  cartReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carouselReducer from './carouselSlice';
 import infoCardsReducer from './infoCardsSlice';
-
+import cartReducer from './cartSlice';
 export const store = configureStore({
   reducer: {
     carousel: carouselReducer,
     infoCards: infoCardsReducer,
+     cart: cartReducer,
   }
 });
 
