3e4d1dae1b342bba3a0e350235d6e89f17167aee
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2025-06-11 18:04:55 -0300

    V 1.7 -  auth Reducer criando arquivo

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import carouselReducer from './carouselSlice';
 import infoCardsReducer from './infoCardsSlice';
 import cartReducer from './cartSlice';
+import authReducer from './authSlice'; 
 export const store = configureStore({
   reducer: {
     carousel: carouselReducer,
     infoCards: infoCardsReducer,
      cart: cartReducer,
+     auth: authReducer 
   }
 });
 
